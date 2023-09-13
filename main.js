05e70070b688ae0cd8eaf931746a9cf51161a43a
zsmith04 <144461629+zsmith04@users.noreply.github.com>
2023-09-13 13:39:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+
+
+
+//Fade the intro text in
+$(document).ready(function(){
+    $(".button").click(function(){
+      $("#div1").fadeToggle(1000);
+      $("#div2").fadeToggle(1000);
+      $("#div3").fadeToggle(3000);
+    });
+  });
+
+  //toggle the additional divs on/off
+  jQuery(document).ready(function (){
+$('#skillsbutton').click(function(){
+	if($("#skills").is(":visible")){
+		$('.content').hide(1000);
+	} else{
+	$('.content').hide(1000);
+    $('#skills').toggle(1000);
+  }});
+  });
+
+  jQuery(document).ready(function (){
+	$('#xpbutton').click(function(){
+		if($("#xp").is(":visible")){
+			$('.content').hide(1000);
+		} else{
+		$('.content').hide(1000);
+	  $('#xp').toggle(1000);
+   }});
+	});
+
+
+	jQuery(document).ready(function (){
+		$('#sitebutton').click(function(){
+			$('.content').hide();
+		  $('#site').toggle(1000);
+	   });
+		});
+
+
+
+		
